6dfd519e10c91c15f62d6fd6f6085664a6e54af2
Oscar Sanchez <osanchez@Oscar-Sanchez.local>
2015-08-09 23:12:45 -0500

    Response for problem 7

---
+++ problem7.rb
@@ -0,0 +1,38 @@
+num_primo = []
+num_actual = 2
+while num_primo.length < 10
+  if (num_actual == 2 or num_actual.modulo(2) != 0)
+    if numero_actual / 2 
+
+    end
+    num_primo.push(num_actual)
+    num_actual += 1
+  else
+    num_actual += 1
+  end
+end
+puts num_primo
+
+
+primos = 1
+i=1
+While primos < 10001:
+    divisires = 0
+For j=1; j<i; j++ :
+    If i%j == 0:
+    Divisores++
+    If divisires < 2:
+    Primos++
+    i++
+
+    Print i
+
+primos = 1
+i = 1
+while primos < 6
+  for j=1, j<i, j++
+
+  end
+end
+
+fir
